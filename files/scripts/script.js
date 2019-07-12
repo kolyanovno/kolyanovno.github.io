0a9855cd37d18e4f7ae1a57469b1dcf898621242
Oksana Polevyk <oksanapolevyk@MacBook-Pro-Oksana.local>
2019-07-13 00:26:30 +0300

    cards logic added

--- files/scripts/script.js
+++ files/scripts/script.js
@@ -48,6 +48,7 @@ for (var g = 0; g <= 3; g++) {
     ul.children[g].children[1].children[0].className = 'stamina-line';
 }
 
+
 //h3
 ul.children[0].children[2].innerText = 'Mordecai';
 ul.children[1].children[2].innerText = 'Roland';
@@ -78,3 +79,50 @@ ul.children[2].children[4].children[0].setAttribute('alt', 'Lilith');
 
 ul.children[3].children[4].children[0].setAttribute('src', 'files/images/brick.png');
 ul.children[3].children[4].children[0].setAttribute('alt', 'Brick');
+
+var listItems = [];
+listItems = document.getElementsByClassName('character-list__item');
+
+var healthList = [];
+healthList = document.getElementsByClassName('health-line');
+
+var healthWidthList = new Array(healthList.length);
+
+for (var i = 0; i < healthWidthList.length; i++) {
+    healthWidthList[i] = 100;
+    healthList[i].style.width = 100 + '%';
+    healthList[i].addEventListener('click', onClickHealts(i));
+}
+
+function onClickHealts(i) {
+    return function () {
+        healthWidthList[i] -= 10;
+        healthList[i].style.width = healthWidthList[i] + '%';
+        console.log(healthList[i].style.width);
+        if (healthList[i].style.width === "0%") {
+            listItems[i].style.display = "none";
+        }
+    }
+}
+
+var staminaList = [];
+staminaList = document.getElementsByClassName('stamina-line');
+
+var staminaWidthList = new Array(staminaList.length)
+
+for (var i = 0; i < staminaWidthList.length; i++) {
+    staminaWidthList[i] = 100;
+    staminaList[i].style.width = 100 + '%';
+    staminaList[i].addEventListener('click', onClickStamina(i));
+}
+
+function onClickStamina(i) {
+    return function () {
+        console.log(staminaList[i].style.width);
+        staminaWidthList[i] -= 10;
+        staminaList[i].style.width = staminaWidthList[i] + '%';
+        if (staminaList[i].style.width === "0%") {
+            listItems[i].style.opacity = ".5";
+        }
+    }
+}
